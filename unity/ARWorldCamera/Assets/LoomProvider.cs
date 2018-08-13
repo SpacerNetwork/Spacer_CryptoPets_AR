85c262578f892e06c8e9b5efa79f4e8b9fffca89
Blueeyer27 <anthonyla2@gmail.com>
2018-08-14 01:28:49 +0300

    Added Loom SDK and made some fixes

---
+++ unity/ARWorldCamera/Assets/LoomProvider.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using Loom.Nethereum.ABI.FunctionEncoding.Attributes;
+using Loom.Client;
+using Loom.Client.Samples;
+using System.Numerics;
+using System.Threading;
+
+[FunctionOutput]
+public class FelineCreation
+{
+    [Parameter("uint", "felineId", 1)]
+    public BigInteger FelineId { get; set; }
+
+    [Parameter("address", "owner", 2)]
+    public string Owner { get; set; }
+    //public GameObject gameObject;
+}
+
+public class LoomProvider : MonoBehaviour
+{
+    void HandleEventHandler(object sender, EvmChainEventArgs e)
+    {
+    }
+
+
+    // Select an ABI from our project resources
+    // We got these from the migration script in Truffle
+    public TextAsset simpleStoreABI;
+    public TextAsset simpleStoreAddress;
+
+    private EvmContract felineContract = null;
+
+    // Use this for initialization
+    public async void Start()
+    {
+        // Generate new keys for this user
+        // TODO - Either store these or let the user enter a private key
+        var privateKey = CryptoUtils.GeneratePrivateKey();
+        var publicKey = CryptoUtils.PublicKeyFromPrivateKey(privateKey);
+
+        // Get the contract
+        felineContract = await GetContract(privateKey, publicKey);
+
+        // Make a call
+        await CreateFeline();
+    }
+
+    // Get's the contract as an object 
+    async Task<EvmContract> GetContract(byte[] privateKey, byte[] publicKey)
+    {
+        var writer = RpcClientFactory.Configure()
+            .WithLogger(Debug.unityLogger)
+            .WithHTTP("http://127.0.0.1:46658/rpc")
+            .Create();
+
+        var reader = RpcClientFactory.Configure()
+            .WithLogger(Debug.unityLogger)
+            .WithHTTP("http://127.0.0.1:46658/query")
+            .Create();
+
+        var client = new DAppChainClient(writer, reader)
+        {
+            Logger = Debug.unityLogger
+        };
+
+        // Required middleware
+        client.TxMiddleware = new TxMiddleware(new ITxMiddlewareHandler[]{
+            new NonceTxMiddleware(publicKey, client),
+            new SignedTxMiddleware(privateKey)
+        });
+
+        // ABI of the Solidity contract
+        string abi = simpleStoreABI.ToString();
+
+        var contractAddr = new Address(simpleStoreAddress.ToString());
+        var callerAddr = Address.FromPublicKey(publicKey);
+
+        return new EvmContract(client, contractAddr, callerAddr, abi);
+    }
+
+    async Task CreateFeline()
+    {
+        AutoResetEvent waitForEvents = new AutoResetEvent(false);
+        //felineContract.EventReceived += onEventReceived;
+
+        await felineContract.CallAsync("createFeline");
+        //await felineContract.CallDtoTypeOutputAsync<FelineCreation>("createFeline");
+
+        //Debug.Log("FelineId " + feline.FelineId);
+        //var value = await felineContract.StaticCallSimpleTypeOutputAsync<int>("getFelineGenes");
+
+        //Debug.Log("CreateFeline = " + value);
+    }
+}
---
+++ unity/ARWorldCamera/Assets/Scripts/Mutation.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mutation : MonoBehaviour
+{
+
+    static Mutation mInstance;
+
+    public static Mutation Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                GameObject go = new GameObject();
+                mInstance = go.AddComponent<Mutation>();
+            }
+            return mInstance;
+        }
+    }
+
+    private int GetGene(string genesStrain, int digits) {
+        var start = genesStrain.Length - digits;
+        var strGene = genesStrain.Substring(start, 2);
+        
+        return int.Parse(strGene);
+    }
+
+    private int GetMagicColor(int gene) {
+        if (gene >= 20) // Chances are 20/80
+            return -1;
+
+        return gene % 11;
+    }
+
+    private int GetSkinColor(int gene) {
+        return gene % 9;
+    }
+
+    private int GetSpotsColor(int gene) {
+        if (gene >= 25) // Chances are 25/75
+            return -1;
+
+        return gene % 7;
+    }
+
+    private PetData GetEmptyFeelineGenes() {
+        var petData = new PetData();
+
+        petData.magicColor = -1;
+        petData.spotsColor = -1;
+        // All other params are equal to 0 by default
+
+        return petData;
+    }
+
+    public PetData GetFeelineGenes(string genesStrain) {
+        if (int.Parse(genesStrain) <= 1)
+            return GetEmptyFeelineGenes();
+
+        var petData = new PetData();
+
+        petData.eyes = GetGene(genesStrain, 2);
+        petData.face = GetGene(genesStrain, 4);
+        petData.ears = GetGene(genesStrain, 6);
+        petData.belly = GetGene(genesStrain, 8);
+        petData.back = GetGene(genesStrain, 10);
+        petData.neck = GetGene(genesStrain, 12);
+        petData.tail = GetGene(genesStrain, 14);
+        petData.legsFront = GetGene(genesStrain, 16);
+        petData.legsBack = GetGene(genesStrain, 18);
+
+        var magicColorGene = GetGene(genesStrain, 20);
+        petData.magicColor = GetMagicColor(magicColorGene);
+
+        var skinColorGene = GetGene(genesStrain, 22);
+        petData.skinColor = GetSkinColor(skinColorGene);
+
+        var spotsColorGene = GetGene(genesStrain, 24);
+        petData.spotsColor = GetSpotsColor(spotsColorGene);
+
+        return petData;
+    }
+}
